f3ec379a4c4f335640b2e450bce11ff69c61c130
Justin Ochoa <just.och92@gmail.com>
2020-10-25 12:11:00 -0400

    added sort by name

--- src/App.js
+++ src/App.js
@@ -6,27 +6,49 @@ function App() {
   const [actor, setActor] = useState([]);
 
   function sortByPop() {
-    console.log("hi");
-    let updatedList = [...contacts].sort((x, y) => y.popularity - x.popularity);
+    let updatedList = [...contacts].sort((a, b) => b.popularity - a.popularity);
     setActor(updatedList);
   }
+
+  function sortByName() {
+    let updatedList = [...contacts].sort(function (a, b) {
+      if (a.name < b.name) {
+        return -1;
+      } else if (a.name > b.name) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+    setActor(updatedList);
+  }
+
   return (
     <div className="App">
       <h1>Iron Contacts</h1>
       <button onClick={sortByPop}>By Popularity</button>
-      {actor.map((eachContact) => {
-        return (
-          <ul>
-            <img
-              src={eachContact.pictureUrl}
-              alt={eachContact.name}
-              style={{ height: "200px" }}
-            />
-            <li>{eachContact.name}</li>
-            <li>{eachContact.popularity}</li>
-          </ul>
-        );
-      })}
+      <button onClick={sortByName}>By Name</button>
+      <table className="table">
+        <tr>
+          <th>Name</th>
+          <th>Picture</th>
+          <th>Rating</th>
+        </tr>
+
+        {actor.map((eachContact) => (
+          <tr key={eachContact.name}>
+            <td>
+              <img
+                src={eachContact.pictureUrl}
+                alt={eachContact.name}
+                style={{ height: "200px" }}
+              />
+            </td>
+            <td>{eachContact.name}</td>
+            <td>{eachContact.popularity}</td>
+          </tr>
+        ))}
+      </table>
     </div>
   );
 }
